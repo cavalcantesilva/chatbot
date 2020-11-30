444763e6662b5edbc2b9e1b52d0f8c44b80acdd1
Humberto-Turioni <70356685+Humberto-Turioni@users.noreply.github.com>
2020-11-30 15:17:38 -0300

    Add files via upload

---
+++ programa_para_gerar_arvore.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+from sklearn import tree
+
+train = pd.read_csv('dados_para_gerar_arvore.csv')
+y_train = train['caro']
+x_train = train.drop(['caro'], axis=1).values
+decision_tree = tree.DecisionTreeClassifier(max_depth = 20)
+decision_tree.fit(x_train, y_train)
+
+with open("27.dot", 'w') as f:
+     f = tree.export_graphviz(decision_tree,
+                              out_file=f,
+                              max_depth = 20,
+                              impurity = True,
+                              feature_names = list(train.drop(['caro'], axis=1)),
+                              class_names = ['False', 'True'],
+                              rounded = True,
+                              filled= True )
+        
